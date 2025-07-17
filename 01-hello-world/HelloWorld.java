8f57c4f0c2ac5ae60d2f2adaf70126d5ace45cb6
Nguyen Ngoc Tam Nhu <nntamnhu01@gmail.com>
2025-07-17 15:25:11 +0700

    Create HelloWorld.java
    Add lesson 01: Hello World

---
+++ 01-hello-world/HelloWorld.java
@@ -0,0 +1,10 @@
+package lesson01_helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // main -> Enter
+        // sout -> Enter
+        System.out.println("Hello, World!");
+        System.out.println(12345);
+    }
+}

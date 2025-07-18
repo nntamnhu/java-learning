dbabb77a29992e4a713badcbb155ef16feb9b208
Nguyen Ngoc Tam Nhu <nntamnhu01@gmail.com>
2025-07-18 16:04:11 +0700

    Create OutputDemo.java
    Add lesson 02: Output example with println, print, newline, tab, and quotes

---
+++ 02-output/OutputDemo.java
@@ -0,0 +1,29 @@
+package lesson02_output;
+
+public class OutputDemo {
+    public static void main(String[] args) {
+
+        // Println
+        System.out.println("First");
+        System.out.println("Second");
+        System.out.println("Third");
+
+        // Print
+        System.out.print("Fourth");
+        System.out.print("Fifth");
+        System.out.println("");
+
+
+        // Xuat ky tu dac biet
+        // \t
+        System.out.println("Cộng hòa xã hội chủ nghĩa Việt Nam");
+        System.out.println("\tĐộc lập - Tự do - Hạnh phúc");
+
+        // \n
+        System.out.println("You don’t have to see the whole staircase\njust take the first step");
+
+        // " "
+        //System.out.println("Some people want it to happen, some wish it would happen, others make it happen");
+        System.out.println("Someone said, \"Discipline is choosing between what you want now and what you want most\"");
+    }
+}

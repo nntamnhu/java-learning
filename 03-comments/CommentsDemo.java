6b7bd3062ad8492cedcc44ffa8c7a0664a162c8d
Nguyen Ngoc Tam Nhu <nntamnhu01@gmail.com>
2025-07-18 16:07:03 +0700

    Create CommentsDemo.java
    Add Lesson 03: Java comments example (// and /* */)

---
+++ 03-comments/CommentsDemo.java
@@ -0,0 +1,23 @@
+package lesson03_comments;
+
+public class CommentsDemo {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello, World!"); // This prints a message to the console
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        System.out.println("This is a demo of comments in Java.");
+
+
+
+        /*
+         * Đây là comment nhiều dòng
+         * dùng để giải thích đoạn mã
+         */
+
+
+    }
+}
